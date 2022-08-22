75181459bbfad32a0ffa02e3c8817a37907d4d67
HD Kim <eastshine94@naver.com>
2022-08-22 20:49:25 +0900

    리액트 훅 기초 익히기1

---
+++ react_concept/basic/src/hook1/1-batch.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  function onClick() {
+    setCount(count + 1);
+    setCount(count + 1);
+  }
+  console.log('render called');
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button onClick={onClick}>증가</button>
+    </div>
+  );
+}
+
+// useEffect(() => {
+//   window.addEventListener('click', onClick);
+//   return () => window.removeEventListener('click', onClick);
+// });
+
+// function onClick() {
+//   ReactDOM.unstable_batchedUpdates(() => {
+//     setCount(v => v + 1);
+//     setCount(v => v + 1);
+//   });
+// }
