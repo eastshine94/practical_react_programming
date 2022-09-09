347114e00928bed8c1f880ffd29858423a721c48
hdkim <eastshine94@naver.com>
2022-09-09 14:34:06 +0900

    2048 game

--- game2048/src/components/AboveGame.js
+++ game2048/src/components/AboveGame.js
@@ -1,14 +1,14 @@
+import React from "react";
+
 export default function AboveGame() {
   return (
     <div className="above-game">
       <p className="game-intro">
-        Join the tiles, get to <strong>2048!</strong>
-        <br />
-        <a href="javascript:void(0)" className="how-to-play-link">
-          How to play →
-        </a>
+        Join the numbers and get to the <strong>2048 tile!</strong>
       </p>
-      <a className="restart-button">New Game</a>
+      <a href="/" className="restart-button">
+        New Game
+      </a>
     </div>
   );
 }
--- game2048/src/components/Game.js
+++ game2048/src/components/Game.js
@@ -1,37 +1,28 @@
-import { useState } from "react";
-
-import { MAX_POS } from "../constant";
+import React, { useState } from "react";
 import times from "lodash/times";
+import useMoveTile from "../hooks/useMoveTile";
 import { getInitialTileList } from "../util/tile";
+import { MAX_POS } from "../constant";
+import Tile from "./Tile";
 
-export default function Game() {
+export default function Game({ setScore }) {
   const [tileList, setTileList] = useState(getInitialTileList);
-
+  useMoveTile({ tileList, setTileList, setScore });
   return (
     <div className="game-container">
-      <div className="game-message">
-        <p></p>
-        <div className="lower">
-          <a className="keep-playing-button">Keep going</a>
-          <a className="retry-button">Try again</a>
-        </div>
-      </div>
       <div className="grid-container">
-        {times(MAX_POS, () => (
-          <div className="grid-row">
-            {times(MAX_POS, () => (
-              <div className="grid-cell"></div>
+        {times(MAX_POS, (y) => (
+          <div key={y} className="grid-row">
+            {times(MAX_POS, (x) => (
+              <div key={y * MAX_POS + x} className="grid-cell"></div>
             ))}
           </div>
         ))}
       </div>
+
       <div className="tile-container">
-        {tileList.map((tile) => (
-          <div
-            className={`tile tile-${tile.value} tile-position-${tile.x}-${tile.y} tile-new`}
-          >
-            <div className="tile-inner">{tile.value}</div>
-          </div>
+        {tileList.map((item) => (
+          <Tile key={item.id} {...item} />
         ))}
       </div>
     </div>
